8b189d4c22583fc6fc4f4876e15609c2206026e8
NotGhoul <58859813+NotGhoull@users.noreply.github.com>
2022-02-15 12:49:33 +0000

    Added some extra info inside of the code

--- classes/generator.py
+++ classes/generator.py
@@ -9,11 +9,19 @@ class Generator:
             str: the password in a string format
         """
 
+        #? Import random
         import random
+
+        #? Create a list of characters
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         upper_alphabet = alphabet.upper()
+
+        #? Create a list of numbers
         numbers = "0123456789"
+
+        #? Create a list of special characters
         special_characters = "!@#$%^&*()_+"
+
         return "".join(
             random.choice(alphabet + upper_alphabet + numbers + special_characters)
             for _ in range(length)
--- gen.py
+++ gen.py
@@ -3,11 +3,16 @@
 # Date: 15/02/2022
 # Description: A simple password generator
 
+#? Import the generator class
 from classes.generator import Generator as gen
 
+#? ask the user for the length
 length = input("Please enter the length of the password\n>>> ")
 
+#? Check whether the length is an integer
 if length == type(int):
+    #? If it is, generate the password
     print(gen.generate_pass(length))
 else:
+    #? If it is not, tell the user to enter an integer
     raise ValueError("Length has to be a number!")
